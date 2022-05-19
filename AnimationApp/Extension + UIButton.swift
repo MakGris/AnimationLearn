3558b944855ff0b4b165c32eba5be94ef4a43ab5
Maks Gri <74944887+MakGris@users.noreply.github.com>
2022-05-19 13:18:36 +0700

    "Add view and button

---
+++ AnimationApp/Extension + UIButton.swift
@@ -0,0 +1,13 @@
+//
+//  Extension + UIButton.swift
+//  AnimationApp
+//
+//  Created by Maksim Grischenko on 19.05.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+}
--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coreAnimationView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func runCoreAnimation(_ sender: UIButton) {
+    }
 }
 
